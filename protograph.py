3cfc7d758cf5d0505407c3edc26f119299bd7b29
Austen Mack-Crane <4mackcrane@gmail.com>
2020-07-09 20:13:46 -0400

    sign, coloring, etc
    Graph now signed (via 'valence' attribute); rendering colored accordingly; interpret as simple graph; restructured plotting

--- protograph.py
+++ protograph.py
@@ -4,7 +4,8 @@ import networkx as nx
 import json
 import plotly.graph_objects as go
 import sys
-
+import numpy as np
+import math
 
 # load
 path = sys.argv[1]
@@ -12,47 +13,98 @@ path = sys.argv[1]
 with open(path, 'r') as f:
     data = json.loads(f.read())
 
-G = nx.readwrite.json_graph.node_link_graph(data, directed=True)
+G = nx.readwrite.json_graph.node_link_graph(data, directed=True, multigraph=False)
 
+if len(sys.argv) > 2:
+    focal_node = sys.argv[2]
+    
 
 # visualize
 
-pos = nx.drawing.layout.planar_layout(G)
+try:
+    pos = nx.drawing.layout.planar_layout(G)
+except nx.exception.NetworkXException:
+    #pos = nx.drawing.layout.spectral_layout(G)
+    pos = nx.drawing.layout.kamada_kawai_layout(G)
+    pos = nx.drawing.layout.spring_layout(G, k=100/math.sqrt(len(G.nodes)), iterations=1000, threshold=2e-3, pos=pos)
 
 
+    
+def arrow(x0, y0, x1, y1):
+    vector = np.array((x1-x0, y1-y0))
+    midpoint = np.array([(x0 + x1)/2, (y1+y0)/2])
+    scale = .05
+    orthpoint = midpoint - scale * vector
+    antivector = scale * np.array([vector[1], -1 * vector[0]])
+    return [orthpoint + antivector, midpoint, orthpoint - antivector,
+            orthpoint + antivector, (None, None)]
+
+    
 # Mostly lifted from plotly docs
-edge_x = []
-edge_y = []
-info = []
+# edited to make edges separate traces s.t. can vary colors
+edges_x = []
+edges_y = []
+texts = []
+edge_colors = []
 for edge in G.edges():
     x0, y0 = pos[edge[0]]
     x1, y1 = pos[edge[1]]
-    edge_x.append(x0)
-    edge_x.append((x0 + x1)/2)
-    edge_x.append(x1)
-    edge_x.append(None)
-    edge_y.append(y0)
-    edge_y.append((y0+y1)/2)
-    edge_y.append(y1)
-    edge_y.append(None)
-    # try throwing text in for hovering?
-    text = G.edges[edge[0], edge[1], 0]['text']
-    info.extend(["", text, "", ""])
+    x = [x0, (x0+x1)/2, x1]
+    y = [y0, (y0+y1)/2, y1]
+    
+    edges_x.append(x)
+    edges_y.append(y)
+    # try throwing text in for hovering?    
+    text = G.edges[edge[0], edge[1]]['text']
+    texts.append(["", text, ""])
+    # color from valence
+    c = int(G.edges[edge[0], edge[1]]['valence'])
+    edge_colors.append(c)
 
-edge_trace = go.Scatter(
-    x=edge_x, y=edge_y,
-    line=dict(width=0.5, color='#888'),
-    hovertemplate='%{text}<extra></extra>', text=info,
-    mode='lines')
+arrows_x = []
+arrows_y = []
+arrow_colors = []
+for edge in G.edges():
+    ar = arrow(*pos[edge[0]], *pos[edge[1]])
+    x,y = zip(*ar)
+    arrows_x.append(x)
+    arrows_y.append(y)
+    # color
+    c = int(G.edges[edge[0], edge[1]]['valence'])
+    arrow_colors.append(c)
+
+# translate to colors
+edge_colors = np.where(np.equal(edge_colors, -1), 'red', np.where(np.equal(edge_colors, 1), 'green', 'gray'))
+arrow_colors = np.where(np.equal(arrow_colors, -1), 'red', np.where(np.equal(arrow_colors, 1), 'green', 'gray'))
+
+edge_traces = []
+for x,y,t,c in zip(edges_x, edges_y, texts, edge_colors):
+    edge_traces.append(go.Scatter(
+        x=x, y=y,
+        line=dict(width=0.5,
+                  color=c
+        ),
+        hovertemplate='%{text}<extra></extra>', text=t,
+        mode='lines'))
+
+arrow_traces = []
+for x,y,c in zip(arrows_x, arrows_y, arrow_colors):
+    arrow_traces.append(go.Scatter(
+        x=x, y=y,
+        line=dict(width=0.5, color=c),
+        mode='lines',
+        hoverinfo='skip'))
 
 node_x = []
 node_y = []
 node_text = []
+node_color = []
 for node in G.nodes():
     x, y = pos[node]
     node_x.append(x)
     node_y.append(y)
     node_text.append(G.nodes[node]['text'])
+    node_color.append(int(G.nodes[node]['valence']))
 
 node_trace = go.Scatter(
     x=node_x, y=node_y,
@@ -62,29 +114,18 @@ node_trace = go.Scatter(
     text=node_text,
     textposition='top center',
     marker=dict(
-        showscale=True,
-        # colorscale options
-        #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
-        #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
-        #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
-        colorscale='YlGnBu',
-        reversescale=True,
-        color=[],
         size=10,
-        colorbar=dict(
-            thickness=15,
-            title='Node Connections',
-            xanchor='left',
-            titleside='right'
-        ),
-        line_width=2))
+        line_width=2,
+        color=node_color,
+        colorscale=['red','gray', 'green'])
+)
 
 
 
 
-fig = go.Figure(data=[edge_trace, node_trace],
+fig = go.Figure(data=arrow_traces + edge_traces + [node_trace],
                 layout=go.Layout(
-                    title='<br>Network graph made with Python',
+                    title='',
                     titlefont_size=16,
                     showlegend=False,
                     hovermode='closest',
